88af5c443ac96ab0e548b3c4e433f7fb4087bee6
Danypooh <delatorrechapell@gmail.com>
2023-11-21 16:51:56 -0600

    realTime DB connection

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,21 @@
-// Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
+import { getDatabase } from 'firebase/database';
 
 // Your web app's Firebase configuration
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyDh0JUb4IkyV7lQuwPp0Lh2VAxwgDdO6JU",
   authDomain: "ga-ulv.firebaseapp.com",
+  databaseURL: "https://ga-ulv-default-rtdb.firebaseio.com",
   projectId: "ga-ulv",
   storageBucket: "ga-ulv.appspot.com",
   messagingSenderId: "51223959286",
   appId: "1:51223959286:web:7caaddcde56f48448abed7"
 };
 
+if (firebase.apps.length == 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = getDatabase();
+
+export { db, firebaseConfig };
